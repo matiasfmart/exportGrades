d4fccf5ce1e64539b57ee1f156fa100cfe5f59c8
Noelia Taboada <noeliataboadavega@gmail.com>
2024-06-19 01:38:14 -0300

    Se corrigen problemas en el formulario settings (Colaboración con Maximiliano Perchik)

--- amd/src/admin.js
+++ amd/src/admin.js
@@ -1,22 +1,33 @@
-define(['jquery'], function($) {
-    return {
-        init: function() {
-            function updateVisibility() {
-                var selectedFrequency = $('#id_s_mod_exportgrades_export_frequency').val();
-                $('.daily-options, .weekly-options, .monthly-options').addClass('hidden');
-                if (selectedFrequency === 'daily') {
-                    $('.daily-options').removeClass('hidden');
-                } else if (selectedFrequency === 'weekly') {
-                    $('.weekly-options').removeClass('hidden');
-                } else if (selectedFrequency === 'monthly') {
-                    $('.monthly-options').removeClass('hidden');
-                }
-            }
-
-            $(document).ready(function() {
-                $('#id_s_mod_exportgrades_export_frequency').change(updateVisibility);
-                updateVisibility(); // Initialize on document ready
-            });
+$(document).ready(function() {
+    $('.settingsform #admin-daily_hour').hide();
+    $('.settingsform #admin-weekly_day,.settingsform #admin-weekly_hour').hide();
+    
+    function updateSettingsDisplay() {
+        const selectedFrequency = $('#id_s_mod_exportgrades_export_frequency').val();
+        if (selectedFrequency === 'daily') {
+            console.log("Entre por el if de daily");
+            $('.settingsform #admin-daily_hour').show();
+            $('.settingsform #admin-weekly_day').hide();
+            $('.settingsform #admin-weekly_hour').hide();
+            $('.settingsform #admin-monthly_day').hide();
+            $('.settingsform #admin-monthly_hour').hide();
+            console.log("Entre a la daily!");
+        } else if (selectedFrequency === 'weekly') {
+            $('.settingsform #admin-daily_hour').hide();
+            $('.settingsform #admin-weekly_day').show();
+            $('.settingsform #admin-weekly_hour').show();
+            $('.settingsform #admin-monthly_day').hide();
+            $('.settingsform #admin-monthly_hour').hide();
+            console.log("Entre a la weekly!");
+        } else {
+            $('.settingsform #admin-daily_hour').hide();
+            $('.settingsform #admin-weekly_day').hide();
+            $('.settingsform #admin-weekly_hour').hide();
+            $('.settingsform #admin-monthly_day').show();
+            $('.settingsform #admin-monthly_hour').show();
         }
-    };
+    }
+
+    updateSettingsDisplay();
+    $('#id_s_mod_exportgrades_export_frequency').change(updateSettingsDisplay);
 });
